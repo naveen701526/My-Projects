c8238a051e504a6c5c1e11d700a2aca63c751251
naveen metta <humanplanet4082443@gmail.com>
2020-09-27 15:34:20 +0530

    2.1.9.1

---
+++ HTML + CSS + Js/Form_Validator/app.js
@@ -0,0 +1,5 @@
+const form=document.getElementById( 'form' );
+const username=document.getElementById( 'username' );
+const email=document.getElementById( 'email' );
+const password=document.getElementById( 'password' );
+const password2=document.getElementById( 'password2' );
