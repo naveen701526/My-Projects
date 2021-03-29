fd81bcc09bd378ba350b8d29b8e632205fcabd26
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-03-29 21:55:23 +0530

    Update max_difference.c

--- C programs/array/max_difference.c
+++ C programs/array/max_difference.c
@@ -2,12 +2,14 @@
 
 #include <stdio.h>
 
+// Function which finds the max difference between two elements
+// where larger element occur after smaller element
 int maximumDifference(int *arr, int size)
 {
-    int min_ele_so_far=arr[0];
-    int maxdiff_so_far=0;
-    int curr_diff,index;
-    for (index=1; index < size; index++)
+    int min_ele_so_far=arr[0];  //Tells the minimum element in the array till the point we scanned.
+    int maxdiff_so_far=0;       //Gives us the result 
+    int curr_diff,index;        //current difference of element inorder to compare with maxdiff_so_far
+    for (index=1; index < size; index++)    //Time complexity is O(n) of this loop
     {
         if (arr [index]< min_ele_so_far)
             min_ele_so_far=arr[index];
