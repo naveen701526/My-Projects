bf19d8f38a3dd73c3b9d137e37cf05c57ec37eeb
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-02 15:35:53 +0530

    Create reverse_string_recursion.c
    Program to Reverse a String Using Recursion

---
+++ C programs/reverse_string_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// declaring recursive function
+char *reverse(char *str);
+
+void main()
+{
+    int i, j, k;
+    char str[100];
+    char *rev;
+    printf("Enter the string:\t");
+    scanf("%s", str);
+    printf("The original string is: %s\n", str);
+    rev = reverse(str);
+    printf("The reversed string is: %s\n", rev);
+}
+
+// defining the function
+char *reverse(char *str)
+{
+    static int i = 0;
+    static char rev[100];
+    if (*str)
+    {
+        reverse(str + 1);
+        rev[i++] = *str;
+    }
+    return rev;
+}
