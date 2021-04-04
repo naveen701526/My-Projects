b0ce638e5e4d3226017a1bc622febc4825c43367
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-04 10:42:02 +0530

    Create number_of_digits_in_number.c
    C program to count number of digits in an integer

---
+++ C programs/number_of_digits_in_number.c
@@ -0,0 +1,29 @@
+/**
+ * C program to count number of digits in an integer
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    long long num;
+    int count = 0;
+
+    /* Input number from user */
+    printf("Enter any number: ");
+    scanf("%lld", &num);
+
+    /* Run loop till num is greater than 0 */
+    do
+    {
+        /* Increment digit count */
+        count++;
+
+        /* Remove last digit of 'num' */
+        num /= 10;
+    } while(num != 0);
+
+    printf("Total digits: %d", count);
+
+    return 0;
+}
