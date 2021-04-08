4fa8e7344f583df9255488461c773531ebcfaa1e
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-08 10:45:31 +0530

    Create length_of_string.py
    Python program to calculate length of a String without using len() function

---
+++ Python /length_of_string.py
@@ -0,0 +1,8 @@
+# User inputs the string and it gets stored in variable str
+str = input("Enter a string: ")
+
+# count variable to count the character in a string
+count = 0
+for s in str:
+    count = count+1
+print("Length of the input string is:", count)
