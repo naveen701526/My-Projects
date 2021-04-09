852756e7ad7d07286fe27696403dcacf778bcdcb
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-09 13:00:30 +0530

    Create anonymous_function.py
    In the program below, we have used an anonymous (lambda) function inside the map() built-in function to find the powers of 2.

---
+++ Python /anonymous_function.py
@@ -0,0 +1,11 @@
+# Display the powers of 2 using anonymous function
+
+
+terms = int(input("How many terms? "))
+
+# use anonymous function
+result = list(map(lambda x: 2 ** x, range(terms)))
+
+print("The total terms are:",terms)
+for i in range(terms):
+   print("2 raised to power",i,"is",result[i])
