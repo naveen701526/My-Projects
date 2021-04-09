eb45a066040c2ea5b0a35314f1009afa43921829
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-09 13:56:52 +0530

    Create quick_sort.py
    Python Program for QuickSort

---
+++ Python /quick_sort.py
@@ -0,0 +1,49 @@
+
+# Implementation performed by using the 
+# last element as the pivot element
+
+
+def partition(arr, low, high):
+    i = (low-1)		 # index of smaller element
+    pivot = arr[high]	 # pivot
+
+    for j in range(low, high):
+
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+
+# Function to do Quick sort
+
+
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+
+        # pi is partitioning index,
+        # arr[p] is at a place where elements to the lhs
+        # and rhs of arr[p] are less and more respectively
+        pi = partition(arr, low, high)
+
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+
+# Driver code to test above
+arr = [int(x) for x in input().split()]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted List: ")
+for i in range(n):
+    print("%d" % arr[i]),
