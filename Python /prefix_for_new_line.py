92355781eb28cae45c4c7592f96301b22eb662aa
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-10 14:02:41 +0530

    Create prefix_for_new_line.py
    A Python program to add a prefix text to all of the lines in a string.

---
+++ Python /prefix_for_new_line.py
@@ -0,0 +1,11 @@
+import textwrap
+sample_text = '''
+    Python is an interpreted, high-level and general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.
+    '''
+text_without_Indentation = textwrap.dedent(sample_text)
+wrapped = textwrap.fill(text_without_Indentation, width=50)
+#wrapped += '\n\nSecond paragraph after a blank line.'
+final_result = textwrap.indent(wrapped, '> ')
+print()
+print(final_result)
+print()
