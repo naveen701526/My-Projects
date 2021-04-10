bf0b992c8901c06420236c5e0667b5e1ab8dc2e6
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-10 14:13:12 +0530

    Create sum_of_digits_in_string.py
    Compute sum of digits of a given string

---
+++ Python /sum_of_digits_in_string.py
@@ -0,0 +1,11 @@
+def sum_digits_string(str1):
+    sum_digit = 0
+    for x in str1:
+        if x.isdigit() == True:
+            z = int(x)
+            sum_digit = sum_digit + z
+
+    return sum_digit
+     
+print(sum_digits_string("123abcd45"))
+print(sum_digits_string("abcd1234"))
