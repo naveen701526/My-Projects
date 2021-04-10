ce3cb989c43fef7caa63b551ece34fda8a4a73b3
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2021-04-10 13:59:53 +0530

    Create round_off_decimals.py
    Print the following floating numbers with no decimal places

---
+++ Python /round_off_decimals.py
@@ -0,0 +1,7 @@
+x = int(input('Enter a positive integer'))
+y = int(input('Enter a negative integer'))
+print("\nOriginal Number: ", x)
+print("Formatted Number with no decimal places: "+"{:.0f}".format(x));
+print("Original Number: ", y)
+print("Formatted Number with no decimal places: "+"{:.0f}".format(y));
+print()
