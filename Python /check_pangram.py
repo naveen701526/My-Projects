95b5d74da742aab62cceb7f06b84897cb2a201c7
naveen <neonew8984@gmail.com>
2021-04-11 16:52:23 +0530

    check panagram program added

---
+++ Python /check_pangram.py
@@ -0,0 +1,11 @@
+import string
+import sys
+
+
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    alphaset = set(alphabet)
+    return alphaset <= set(str1.lower())
+
+
+print(ispangram('The quick brown fox jumps over the lazy dog'))
+print(ispangram('This line is definitely not a panagram.'))
