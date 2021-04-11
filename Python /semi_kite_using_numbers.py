9016810d9c5635b296ed5133557f7cb8fdfbe8e5
naveen <neonew8984@gmail.com>
2021-04-11 16:57:51 +0530

    semi kite program using numbers  added

---
+++ Python /semi_kite_using_numbers.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(n):
+    for j in range(i):
+        print(str(j+1) + ' ', end="")
+    print('')
+
+for i in range(n, 0, -1):
+    for j in range(i):
+        print(str(j + 1) + ' ', end="")
+    print('')
