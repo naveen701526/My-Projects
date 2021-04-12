77b43154cd833b93d8936a598ed28c9012689672
naveen <neonew8984@gmail.com>
2021-04-12 14:21:19 +0530

    music player css complete

---
+++ HTML + CSS + Js/music player/script.js
@@ -0,0 +1,31 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+
+
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+
+// Keep track of song
+let songIndex = 2;
+
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
