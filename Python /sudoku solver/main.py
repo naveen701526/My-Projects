f0c483482fdf733f5e8d4a8388b08869cb2117dc
naveen <neonew8984@gmail.com>
2021-04-13 12:11:14 +0530

    sudoku solver  check valid funtion

--- Python /sudoku solver/main.py
+++ Python /sudoku solver/main.py
@@ -11,6 +11,27 @@ def find_next_empty(puzzle):
     return None, None  # if no spaces in the puzzle are empty (-1)
 
 
+def is_valid(puzzle, guess, row, col):
+    '''figures out whether the guess at the row/col of the puzzle is a valid guess returns true if is valid, false other wise'''
+
+    # let's start with the row
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+
+    # now the column
+    # col_vals = []
+    # for i in range(9):
+    #     col_vals.append(puzzle[i][col])
+    col_vals = [puzzle[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+
+    # and then the square
+    row_start = (row // 3) * 3  # 10 // 3 = 3, 5 // 3 = 1, 1 // 3 = 0
+    col_start = (col // 3) * 3
+
+
 def solve_sudoku(puzzle):
     ''' solve sudoku using backtracking!
     the puzzle parameter will be a list of lists, 
@@ -20,3 +41,13 @@ def solve_sudoku(puzzle):
 
     # choose a block in puzzle to place the value
     row, col = find_next_empty(puzzle)
+
+    # if there's no where left, then we're done because we only allowed
+
+    if row is None:
+        return True
+
+    # if there is a place to put a number, then make a guess between a number 1 to 9
+    for guess in range(1, 10):  # range(1,10) is 1,2,3,...9
+        # check if it is a valid guess
+        if is_valid(puzzle, guess, r)
