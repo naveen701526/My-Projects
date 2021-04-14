ab3afb6b25f49c3a191d065bc06d5d7f9eaf82c8
naveen <neonew8984@gmail.com>
2021-04-14 12:06:06 +0530

    color value generator event listener to button

--- HTML + CSS + Js/color value generator/script.js
+++ HTML + CSS + Js/color value generator/script.js
@@ -2,3 +2,16 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 
 const btn = document.querySelector("#btn");
+
+const color = document.querySelector(".color");
+
+
+btn.addEventListener("click", () => {
+//   const randomNumber = getRandomNumber();
+//   console.log(randomNumber);
+    // get random number between 0 - 3
+    // because array are zero index based
+const randomNumber =  2
+//   document.body.style.backgroundColor = colors[randomNumber];
+//   color.textContent = colors[randomNumber];
+});
