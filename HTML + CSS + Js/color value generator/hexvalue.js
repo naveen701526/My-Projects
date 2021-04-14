de4e1e3cd7bb6fa05a1f45a31bd18cf2916af3b3
naveen <neonew8984@gmail.com>
2021-04-14 13:08:08 +0530

    color value generator javascript of hexvalue which all references to the dom elements

--- HTML + CSS + Js/color value generator/hexvalue.js
+++ HTML + CSS + Js/color value generator/hexvalue.js
@@ -3,3 +3,19 @@
 // 0-9 will be represent as decimal number system
 // from 10 to 15 we will represent it using A to F
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+// the variable btn will refer to element button
+// which has btn as id
+const btn = document.querySelector('#btn')
+
+// the variable color will refer to element h2 
+// which has class name as color
+const color = document.querySelector('.color')
+
+
+// this is an event listener which will do something
+// when the button in the page is clicked
+btn.addEventListener('click', () => {
+    let hexColor = '#'
+})
