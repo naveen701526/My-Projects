4e045630e42fa732768c0b151d67c41e3f8fc99b
naveen <neonew8984@gmail.com>
2021-04-14 13:01:42 +0530

    color value generator javascript of hexvalue html

---
+++ HTML + CSS + Js/color value generator/hexvalue.js
@@ -0,0 +1,5 @@
+// range of values that the hex decimal can take
+// which is 0 to 15
+// 0-9 will be represent as decimal number system
+// from 10 to 15 we will represent it using A to F
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
