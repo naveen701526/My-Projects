a21f0ab76748dea32bf6994ceb662fad1e48869d
naveen <neonew8984@gmail.com>
2021-04-15 10:38:14 +0530

    counting app creating variables value and btns

--- HTML + CSS + Js/counting app in js/script.js
+++ HTML + CSS + Js/counting app in js/script.js
@@ -1,2 +1,13 @@
 // set initial count
 let count = 0;
+
+
+// select value and buttons
+// value variable refers to 
+// an element with value as its id
+const value = document.querySelector('#value')
+
+// btns variable refers to an element with btn as its class name
+const btns = document.querySelectorAll('.btn')
+
+
