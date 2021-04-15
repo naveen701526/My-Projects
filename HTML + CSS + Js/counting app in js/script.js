9888ef63e6134c94ea439015e92c98088b693f34
naveen <neonew8984@gmail.com>
2021-04-15 10:48:09 +0530

    counting app increase funtionality

--- HTML + CSS + Js/counting app in js/script.js
+++ HTML + CSS + Js/counting app in js/script.js
@@ -17,6 +17,12 @@ btns.forEach((btn) => {
         if (styles.contains('decrease')) {
             count--;
         }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+        else if (styles.contains('reset')) {
+            count = 0
+        }
         value.textContent = count
     })
 })
