6f601479b90059ec5c1f546c9bb0d9cfc8af7276
naveen <neonew8984@gmail.com>
2021-04-16 11:54:03 +0530

    tetris game refer the grid div and small div inside it using js

--- HTML + CSS + Js/tetris game/script.js
+++ HTML + CSS + Js/tetris game/script.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+    
 
 
 })
