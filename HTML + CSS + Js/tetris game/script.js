cd1812b3e276a9a90dfabf4c7baaff2865db42bb
naveen <neonew8984@gmail.com>
2021-04-16 12:20:50 +0530

    tetris game give shape to tetrominoes

--- HTML + CSS + Js/tetris game/script.js
+++ HTML + CSS + Js/tetris game/script.js
@@ -9,6 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('#start-button')
     const width = 10
     
-
+      //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
 
 })
