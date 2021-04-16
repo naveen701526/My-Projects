ed3e545722335f5bd5697fc45a9c2404f3e96554
naveen <neonew8984@gmail.com>
2021-04-16 12:03:43 +0530

    tetris game refer to all objects in dom using js

--- HTML + CSS + Js/tetris game/script.js
+++ HTML + CSS + Js/tetris game/script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
+
+    const scoreDisplay = document.querySelector('#score')
+    
+    const startBtn = document.querySelector('#start-button')
     const width = 10
     
 
