1cd0e6405b686b1664a6b7aee927a0a8ce4499c0
naveen <neonew8984@gmail.com>
2021-04-16 12:42:11 +0530

    tetris game creat ztetromino and ttetromino

--- HTML + CSS + Js/tetris game/script.js
+++ HTML + CSS + Js/tetris game/script.js
@@ -17,4 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width*2, width*2+1, width*2+2]
   ]
 
+    const zTetromino = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+    ]
+    
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+        
+    ]
 })
