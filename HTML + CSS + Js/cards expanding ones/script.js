03cd6849be145d74a22102cba3892b43e5060bdb
naveen <neonew8984@gmail.com>
2021-04-17 12:56:31 +0530

    expanding cards remove active class functionality

--- HTML + CSS + Js/cards expanding ones/script.js
+++ HTML + CSS + Js/cards expanding ones/script.js
@@ -9,3 +9,10 @@ panels.forEach(panel => {
         panel.classList.add('active')
     })
 })
+
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
