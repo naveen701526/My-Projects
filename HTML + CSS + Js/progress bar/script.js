b552b114d1d0626001a6a2996cb3da123657baad
naveen <neonew8984@gmail.com>
2021-04-18 14:30:10 +0530

    progress bar event listener for next button

--- HTML + CSS + Js/progress bar/script.js
+++ HTML + CSS + Js/progress bar/script.js
@@ -4,3 +4,17 @@ const progress = document.querySelector('#progress')
 const prev = document.querySelector('#prev')
 const next = document.querySelector('#next')
 const circles = document.querySelectorAll('.circle')
+
+
+let currentActive = 1
+
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
+})
