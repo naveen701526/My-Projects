680c01c63a71faa75c4e0b9c01d3019810e01d63
naveen <neonew8984@gmail.com>
2021-04-18 14:27:54 +0530

    progress bar refer to required objects in dom using js

---
+++ HTML + CSS + Js/progress bar/script.js
@@ -0,0 +1,6 @@
+
+const progress = document.querySelector('#progress')
+
+const prev = document.querySelector('#prev')
+const next = document.querySelector('#next')
+const circles = document.querySelectorAll('.circle')
