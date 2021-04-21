5b2f72cd70d39bfd75704f62a0d7059ce183c779
naveen <neonew8984@gmail.com>
2021-04-21 12:39:23 +0530

    blur loading page change the percentage

--- HTML + CSS + Js/blur load page/script.js
+++ HTML + CSS + Js/blur load page/script.js
@@ -1,2 +1,17 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
+
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+    // console.log(load);
+    loadText.innerText = `${load}%`
+}
