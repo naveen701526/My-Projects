c3cbe1f89153fd1defc90da881b39e0830d9fbbf
naveen <neonew8984@gmail.com>
2021-04-21 12:57:33 +0530

    blur loading page complete version

--- HTML + CSS + Js/blur load page/script.js
+++ HTML + CSS + Js/blur load page/script.js
@@ -15,7 +15,7 @@ function blurring() {
     // console.log(load);
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
-
+     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
 
