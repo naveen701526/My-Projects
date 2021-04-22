be91a209124674df3ca9c728ab70ea932780478b
naveen <neonew8984@gmail.com>
2021-04-22 14:04:48 +0530

    complete version of lightbox

--- HTML + CSS + Js/lightbox/script.js
+++ HTML + CSS + Js/lightbox/script.js
@@ -6,6 +6,14 @@ images.forEach(image => {
         lightbox.classList.add('active')
         const img = document.createElement('img')
         img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
         lightbox.appendChild(img)
     })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
 })
