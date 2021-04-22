60b254c42a19a9f6403610e72afad66f4df2a015
naveen <neonew8984@gmail.com>
2021-04-22 12:34:14 +0530

    get viewport place of each box

--- HTML + CSS + Js/scroll effect/script.js
+++ HTML + CSS + Js/scroll effect/script.js
@@ -7,5 +7,11 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes() {
-    console.log(window.innerHeight);
+    const triggerBottom = window.innerHeight /5 *4;
+
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect()
+    })
 }
+
