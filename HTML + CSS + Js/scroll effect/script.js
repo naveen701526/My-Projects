643652cb65ccf55c09e3bfbe19f295c1945ac3a6
naveen <neonew8984@gmail.com>
2021-04-22 12:29:23 +0530

    check window height

--- HTML + CSS + Js/scroll effect/script.js
+++ HTML + CSS + Js/scroll effect/script.js
@@ -1,3 +1,11 @@
 const boxes = document.querySelectorAll('.box')
 
 
+window.addEventListener('scroll', checkBoxes)
+
+
+checkBoxes()
+
+function checkBoxes() {
+    console.log(window.innerHeight);
+}
