743de26f697aca47be9cf925da299d5f12855db2
naveen <neonew8984@gmail.com>
2021-04-23 16:03:33 +0530

    add eventlistener on both images

---
+++ HTML + CSS + Js/split page/script.js
@@ -0,0 +1,13 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
