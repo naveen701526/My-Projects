ede358938f1a2c7a5b955a6ad07f880007b1acf0
naveen <neonew8984@gmail.com>
2021-04-23 16:05:01 +0530

    fix error

--- HTML + CSS + Js/split page/script.js
+++ HTML + CSS + Js/split page/script.js
@@ -9,5 +9,5 @@ left.addEventListener('mouseleave', () => container.classList.remove('hover-left
 
 
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
