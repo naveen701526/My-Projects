c44f61af7707bb9a5866ab16d909d9585c6499bb
naveen <neonew8984@gmail.com>
2021-04-25 14:08:30 +0530

    display buttons to play song

---
+++ HTML + CSS + Js/sound board/script.js
@@ -0,0 +1,24 @@
+const sounds = [
+    'blank',
+    'fade',
+    'invincible',
+    'myheart',
+    'onandon',
+    'spectre'
+]
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+
+    btn.innerText = sound
+
+    // btn.addEventListener('click', () => {
+        
+
+
+    // })
+
+    document.querySelector('#buttons').appendChild(btn)
+})
