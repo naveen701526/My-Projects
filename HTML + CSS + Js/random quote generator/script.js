eb55bc110e89353177a30c7084f54491dcbb12cd
naveen <neonew8984@gmail.com>
2021-04-26 18:00:19 +0530

    display data

--- HTML + CSS + Js/random quote generator/script.js
+++ HTML + CSS + Js/random quote generator/script.js
@@ -2,12 +2,18 @@ const btn = document.querySelector('#btn')
 
 const quotes = document.querySelector('.quotes')
 
+
+
+
 generateQuote()
+
+
+
 
 
 
 function generateQuote() {
     fetch('http://quotes.stormconsultancy.co.uk/random.json')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => quotes.innerText=data.quote);
 }
