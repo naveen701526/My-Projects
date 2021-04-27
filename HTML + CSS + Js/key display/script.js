60a7419e6a4fba385c8c782486c1f50298fcdfb6
naveen <neonew8984@gmail.com>
2021-04-27 13:48:14 +0530

    add div in wrapper div using js

--- HTML + CSS + Js/key display/script.js
+++ HTML + CSS + Js/key display/script.js
@@ -1,10 +1,25 @@
 const insert = document.querySelector('#insert')
 
 window.addEventListener('keydown', event => {
-    // insert.innerHTML = `
+    insert.innerHTML = `
+        <div class="key">
+        ${event.key = ' ' ? 'space' : event.key}
+        <small>event.key</small>
+        </div>
 
+        <div class="key">
+        ${event.keyCode}
+        <small>event.keyCode</small>
+        </div>
+
+        <div class="key">
+        ${event.code}
+        <small>event.code</small>
+        </div>
+
+        
     
-    // `
+    `
     // console.log(event);
 
 })
