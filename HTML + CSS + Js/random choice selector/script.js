77abc4d523c416ba53ad9930a5e32daec6c01581
naveen <neonew8984@gmail.com>
2021-04-29 13:17:58 +0530

    create tags

---
+++ HTML + CSS + Js/random choice selector/script.js
@@ -0,0 +1,26 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+
+textarea.focus()
+
+
+textarea.addEventListener('keyup', e => {
+    createTags(e.target.value)
+})
+
+
+
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+     tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+}
