11efe6f35508d8a799dbd7295e1088e91e45bdcc
naveen <neonew8984@gmail.com>
2021-04-29 13:23:22 +0530

    complete version of project

--- HTML + CSS + Js/random choice selector/script.js
+++ HTML + CSS + Js/random choice selector/script.js
@@ -72,3 +72,15 @@ function pickRandomTag() {
     const tags = document.querySelectorAll('.tag')
     return tags[Math.floor(Math.random() * tags.length)]
 }
+
+
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
