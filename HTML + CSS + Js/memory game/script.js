546043a67d31218b53b7c4b1f78dbd559d62d4a5
naveen <neonew8984@gmail.com>
2021-05-01 16:25:05 +0530

    function to display images on board

--- HTML + CSS + Js/memory game/script.js
+++ HTML + CSS + Js/memory game/script.js
@@ -1,53 +1,69 @@
 document.addEventListener('DOMContentLoaded', () => {
     // cards which will get displayed
-     const cardArray = [
-    {
-      name: 'fries',
-      img: 'images/fries.png'
-    },
-    {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
-    },
-    {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
-    },
-    {
-      name: 'pizza',
-      img: 'images/pizza.png'
-    },
-    {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
-    },
-    {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
-    },
-    {
-      name: 'fries',
-      img: 'images/fries.png'
-    },
-    {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
-    },
-    {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
-    },
-    {
-      name: 'pizza',
-      img: 'images/pizza.png'
-    },
-    {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
-    },
-    {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png',
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png',
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png',
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png',
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png',
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png',
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png',
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png',
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png',
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png',
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png',
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png',
+        },
+    ];
+
+    const grid = document.querySelector('.grid');
+
+    // function which will create the board when the page is loaded
+
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            // card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+        }
     }
-  ]
-})
+
+    createBoard();
+});
