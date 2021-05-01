7748c1fbd61055c924a2108f58123b7b4b3163b1
naveen <neonew8984@gmail.com>
2021-05-01 16:20:21 +0530

    add images in js file

---
+++ HTML + CSS + Js/memory game/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // cards which will get displayed
+     const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    }
+  ]
+})
