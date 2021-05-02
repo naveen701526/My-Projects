1fba33df15c719ca0633f058447b334eaf7fc3df
naveen <neonew8984@gmail.com>
2021-05-02 18:17:39 +0530

    complete version of project

--- HTML + CSS + Js/whac a mole/script.js
+++ HTML + CSS + Js/whac a mole/script.js
@@ -9,6 +9,7 @@ const score = document.querySelector('#score');
 let result = 0;
 let hitPosition;
 let timerId = null;
+let currentTime = 60;
 
 function randomSquare() {
     squares.forEach((square) => {
@@ -18,6 +19,35 @@ function randomSquare() {
     let randomSquare = squares[Math.floor(Math.random() * 9)];
 
     randomSquare.classList.add('mole');
+    hitPosition = randomSquare.id;
 }
 
-setInterval(randomSquare, 500);
+squares.forEach((square) => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++;
+            score.textContent = result;
+            console.log('hi');
+            hitPosition = null;
+        }
+    });
+});
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500);
+}
+
+moveMole();
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId);
+        clearInterval(timerId);
+        alert('GAME OVER! Your final score is ' + result);
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
