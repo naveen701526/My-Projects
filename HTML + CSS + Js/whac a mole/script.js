34d5f2408c8f3e215ae9cd517e787063b10a20a2
naveen <neonew8984@gmail.com>
2021-05-02 17:30:55 +0530

    refer to essential element using js

---
+++ HTML + CSS + Js/whac a mole/script.js
@@ -0,0 +1,7 @@
+const squares = document.querySelectorAll('.square');
+
+const mole = document.querySelector('.mole');
+
+const timeLeft = document.querySelector('#time-left');
+
+const score = document.querySelector('#score');
