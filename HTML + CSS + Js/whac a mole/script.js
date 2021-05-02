dd04ccac7147a40528bae6a82b49869f4ee950c0
naveen <neonew8984@gmail.com>
2021-05-02 17:46:28 +0530

    place mole in random box

--- HTML + CSS + Js/whac a mole/script.js
+++ HTML + CSS + Js/whac a mole/script.js
@@ -5,3 +5,19 @@ const mole = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-left');
 
 const score = document.querySelector('#score');
+
+let result = 0;
+let hitPosition;
+let timerId = null;
+
+function randomSquare() {
+    squares.forEach((square) => {
+        square.classList.remove('mole');
+    });
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+
+    randomSquare.classList.add('mole');
+}
+
+setInterval(randomSquare, 500);
