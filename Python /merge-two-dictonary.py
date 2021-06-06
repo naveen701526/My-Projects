08c4acc87cbb6aae3965a9aef1cf4663b7a4cdeb
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-06-06 14:40:49 +0530

    Create merge-two-dictonary.py

---
+++ Python /merge-two-dictonary.py
@@ -0,0 +1,14 @@
+#Using operator **
+
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print({**dict_1, **dict_2})
+
+
+# Using operator |
+
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print(dict_1|dict_2)
