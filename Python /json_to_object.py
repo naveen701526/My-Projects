dc9021cb8822a5ab2305ee0290301c9fb09bd8a3
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-07-04 11:05:09 +0530

    Create json_to_object.py

---
+++ Python /json_to_object.py
@@ -0,0 +1,8 @@
+import json
+json_obj =  '{ "Name":"naveen", "Class":"I", "Age":19 }'
+python_obj = json.loads(json_obj)
+print("\nJSON data:")
+print(python_obj)
+print("\nName: ",python_obj["Name"])
+print("Class: ",python_obj["Class"])
+print("Age: ",python_obj["Age"]) 
