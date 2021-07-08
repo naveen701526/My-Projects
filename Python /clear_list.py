0625efeb9780dd69bccead6435a832806f56f015
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-07-08 21:50:55 +0530

    Create clear_list.py

---
+++ Python /clear_list.py
@@ -0,0 +1,10 @@
+# Python program to clear a list
+# using clear() method
+
+# Creating list
+GEEK = [6, 0, 4, 1]
+print('GEEK before clear:', GEEK)
+
+# Clearing list
+GEEK.clear()
+print('GEEK after clear:', GEEK)
