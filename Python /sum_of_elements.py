2f8a1e5c3525b554767a64b392ab877e885b4572
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-07-09 16:29:47 +0530

    Create sum_of_elements.py

---
+++ Python /sum_of_elements.py
@@ -0,0 +1,13 @@
+# Python program to find sum of elements in list
+total = 0
+
+# creating a list
+list1 = [11, 5, 17, 18, 23]
+
+# Iterate each element in list
+# and add them in variable total
+for ele in range(0, len(list1)):
+	total = total + list1[ele]
+
+# printing total value
+print("Sum of all elements in given list: ", total)
