6d8044ca89809b1bc52071ba4305489dc3e3875b
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-07-09 16:24:32 +0530

    Create largest_element_list.py

---
+++ Python /largest_element_list.py
@@ -0,0 +1,16 @@
+# Python program to multiply all values in the
+# list using traversal
+
+def multiplyList(myList) :
+	
+	# Multiply elements one by one
+	result = 1
+	for x in myList:
+		result = result * x
+	return result
+	
+# Driver code
+list1 = [1, 2, 3]
+list2 = [3, 2, 4]
+print(multiplyList(list1))
+print(multiplyList(list2))
