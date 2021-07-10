b873bdce9b07341390024e0efb49cd4c00fb1fbc
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-07-10 14:25:40 +0530

    Create print_positive_range.py

---
+++ Python /print_positive_range.py
@@ -0,0 +1,10 @@
+# Python program to print positive Numbers in given range
+
+start, end = -4, 19
+
+# iterating each number in list
+for num in range(start, end + 1):
+	
+	# checking condition
+	if num >= 0:
+		print(num, end = " ")
