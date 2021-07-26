7890351ded193048d6435c424a70f5026acc0a2a
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-07-26 18:26:20 +0530

    Create array_manipulation.py

---
+++ Python /array_manipulation.py
@@ -0,0 +1,18 @@
+# Problem Statement: https://www.hackerrank.com/challenges/crush/problem
+
+import itertools
+import sys
+
+
+if __name__ == '__main__':
+    N, M = list(map(int, sys.stdin.readline().split()))
+    x = [0] * N
+
+    for _ in range(M):
+        a, b, k = list(map(int, sys.stdin.readline().split()))
+
+        x[a - 1] += k
+        if b < N:
+            x[b] -= k
+
+    print(max(itertools.accumulate(x)))
