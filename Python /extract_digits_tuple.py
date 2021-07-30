89e26cc091308314eb48294195724b17b93863fe
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-07-30 22:27:08 +0530

    Create extract_digits_tuple.py

---
+++ Python /extract_digits_tuple.py
@@ -0,0 +1,21 @@
+# Python3 code to demonstrate working of
+# Extract digits from Tuple list
+# Using map() + chain.from_iterable() + set() + loop
+from itertools import chain
+
+# initializing list
+test_list = [(15, 3), (3, 9), (1, 10), (99, 2)]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# Extract digits from Tuple list
+# Using map() + chain.from_iterable() + set() + loop
+temp = map(lambda ele: str(ele), chain.from_iterable(test_list))
+res = set()
+for sub in temp:
+	for ele in sub:
+		res.add(ele)
+
+# printing result
+print("The extrated digits : " + str(res))
