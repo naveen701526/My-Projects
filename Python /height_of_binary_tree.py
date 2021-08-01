700f7d0d8558793b109515e336651f8c9249f3ca
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-08-01 23:21:20 +0530

    Create height_of_binary_tree.py

---
+++ Python /height_of_binary_tree.py
@@ -0,0 +1,40 @@
+# Python3 program to find the maximum depth of tree
+
+# A binary tree node
+class Node:
+
+	# Constructor to create a new node
+	def __init__(self, data):
+		self.data = data
+		self.left = None
+		self.right = None
+
+# Compute the "maxDepth" of a tree -- the number of nodes
+# along the longest path from the root node down to the
+# farthest leaf node
+def maxDepth(node):
+	if node is None:
+		return 0 ;
+
+	else :
+
+		# Compute the depth of each subtree
+		lDepth = maxDepth(node.left)
+		rDepth = maxDepth(node.right)
+
+		# Use the larger one
+		if (lDepth > rDepth):
+			return lDepth+1
+		else:
+			return rDepth+1
+
+
+# Driver program to test above function
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+
+print("Height of tree is %d" %(maxDepth(root)))
