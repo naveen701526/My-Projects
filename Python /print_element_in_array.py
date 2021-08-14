603750809864b6ad19a4d085ce21a3558a8b3283
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-08-14 18:55:53 +0530

    Create print_element_in_array.py

---
+++ Python /print_element_in_array.py
@@ -0,0 +1,8 @@
+#Initialize array     
+arr = [1, 2, 3, 4, 5];     
+     
+print("Elements of given array: ");    
+#Loop through the array by incrementing the value of i     
+    
+for i in range(0, len(arr)):    
+    print(arr[i])
