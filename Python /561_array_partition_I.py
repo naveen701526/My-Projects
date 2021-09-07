eda5eb67849c41ae837ee4b994582ff906f3bf51
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-09-07 23:13:48 +0530

    Create 561_array_partition_I.py

---
+++ Python /561_array_partition_I.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(0,len(nums),2):
+            ans += nums[i]
+        return ans
+        
+        
+opt = Solution()
+print(opt.arrayPairSum([1,4,3,2]))
