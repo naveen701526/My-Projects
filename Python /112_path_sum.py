e1ecf7b51a0e951d0cfaa847b2780cad83b59523
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-09-13 21:46:49 +0530

    Create 112_path_sum.py

---
+++ Python /112_path_sum.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def traversal(self, node, cur_sum, target_sum):
+        if node:
+            if not node.left and not node.right and cur_sum + node.val == target_sum:
+                return True
+        
+            return self.traversal(node.left, cur_sum + node.val, target_sum) or self.traversal(node.right, cur_sum + node.val, target_sum)
+        
+        return False
+        
+    def hasPathSum(self, root, targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        return self.traversal(root, 0, targetSum)
+        
+        
+        
+opt = Solution()
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+print(opt.hasPathSum(root, 4))
