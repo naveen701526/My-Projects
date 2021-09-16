803fc11aa14ec7f17c42121ce92833c4019ece25
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-09-16 23:18:09 +0530

    Create gcd_of_more_than_two_numbers.py

---
+++ Python /gcd_of_more_than_two_numbers.py
@@ -0,0 +1,22 @@
+# GCD of more than two (or array) numbers
+
+# Function implements the Euclidian
+# algorithm to find H.C.F. of two number
+def find_gcd(x, y):
+	
+	while(y):
+		x, y = y, x % y
+	
+	return x
+		
+# Driver Code	
+l = [2, 4, 6, 8, 16]
+
+num1 = l[0]
+num2 = l[1]
+gcd = find_gcd(num1, num2)
+
+for i in range(2, len(l)):
+	gcd = find_gcd(gcd, l[i])
+	
+print(gcd)
