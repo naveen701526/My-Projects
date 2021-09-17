609c4d60161629ab523bcbace3e7390e91774a04
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-09-17 23:08:51 +0530

    Create minimum_rotation_to_get_same_string.py

---
+++ Python /minimum_rotation_to_get_same_string.py
@@ -0,0 +1,30 @@
+# Python 3 program to determine minimum
+# number of rotations required to yield
+# same string.
+
+# Returns count of rotations to get the
+# same string back.
+def findRotations(str):
+	
+	# tmp is the concatenated string.
+	tmp = str + str
+	n = len(str)
+
+	for i in range(1, n + 1):
+		
+		# substring from i index of
+		# original string size.
+		substring = tmp[i: i+n]
+
+		# if substring matches with
+		# original string then we will
+		# come out of the loop.
+		if (str == substring):
+			return i
+	return n
+
+
+
+
+str = "abc"
+print(findRotations(str))
