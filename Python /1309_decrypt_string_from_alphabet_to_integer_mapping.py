44bc9c0ab8acfcba6ff729c32ba91d3be0c8a9f8
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-09-29 23:05:49 +0530

    Update 1309_decrypt_string_from_alphabet_to_integer_mapping.py

--- Python /1309_decrypt_string_from_alphabet_to_integer_mapping.py
+++ Python /1309_decrypt_string_from_alphabet_to_integer_mapping.py
@@ -1,14 +1,14 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
         ans = ""
-        i = len(s)-1
+        i = len(s)-1 #pointer to end character of string  
         while i>=0:
-            if s[i]=="#":
-                ans = ans + chr(int(s[i-2:i])+96)
-                i=i-2
-            else:
-                ans = ans + chr(int(s[i])+96)
-            i -= 1
+            if s[i]=="#": #encounter '#' means character > I
+                ans = ans + chr(int(s[i-2:i])+96) #add two characters with base 96
+                i=i-2 #hence jump two characters in string
+            else: 
+                ans = ans + chr(int(s[i])+96) #case for adding single digit number 
+            i -= 1 
         return ans[::-1]
         
 opt = Solution()
