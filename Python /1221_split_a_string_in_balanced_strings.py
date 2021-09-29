9b35de32ebfe45fca3183f1c8cdf8b65754c31b7
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-09-29 22:47:46 +0530

    Update 1221_split_a_string_in_balanced_strings.py

--- Python /1221_split_a_string_in_balanced_strings.py
+++ Python /1221_split_a_string_in_balanced_strings.py
@@ -1,18 +1,18 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        c=0
-        g=0
-        x=''
+        c=0 #counts number of same characters
+        g=0 #counts number of splits according to problem statement
+        x='' #first character from where the split starts
         for i in s:
-            if(c==0):
-                g+=1
-                x=i
+            if(c==0): #meaning no split has begun yet
+                g+=1  #meaning we have found a split
+                x=i   #first character of the split
+                c+=1  #count of similar character could be either L/R
+            elif(i==x): #current character equals previous character then increase the count c
                 c+=1
-            elif(i==x):
-                c+=1
-            else:
+            else:   #when L/R meet their opposite 
                 c-=1
-        return g
+        return g #returns the number of splits
         
 opt = Solution()
 print(opt.balancedStringSplit(s = "RLRRLLRLRL"))
