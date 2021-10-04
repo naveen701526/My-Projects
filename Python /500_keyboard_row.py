82edb5b6990d60e64a5600fbbbebe36734054ea1
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-10-04 14:47:02 +0530

    Update 500_keyboard_row.py

--- Python /500_keyboard_row.py
+++ Python /500_keyboard_row.py
@@ -14,21 +14,22 @@ class Solution:
         output = []
         for word in words:
             
-            if word[0] in row_1:
-                val = self.check_row(word, row_1)
+            if word[0] in row_1: #check if first letter is in row_1
+                val = self.check_row(word, row_1) #check if entire word is in row_1
                 if val:
-                    output.append(word)
-            elif word[0] in row_2:
+                    output.append(word) 
+            elif word[0] in row_2: #check if first letter is in row_2
                 
-                val = self.check_row(word, row_2)
+                val = self.check_row(word, row_2) #check if entire word is in row_2
                 if val:
                     output.append(word)
-            elif word[0] in row_3:
+            elif word[0] in row_3: #check if first letter is in row_3
                 
-                val = self.check_row(word, row_3)
+                val = self.check_row(word, row_3) #check if entire word is in row_3
                 if val:
                     output.append(word)
-        return output
+        return output #gives the words which either belong to row_1 or row_2 or row_3
         
 opt = Solution()
 print(opt.findWords(["qwertyuiopQWERTYUIOP"]))
+print(opt.findWords(words = ["Hello","Alaska","Dad","Peace"]))
