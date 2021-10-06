e435dde69ffc0aac425aa34ece9f5aa70ff92711
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-10-06 21:44:52 +0530

    Create 70_climbing_stairs.py

---
+++ Python /70_climbing_stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+       #tabulation
+        if n<=2:
+            return n
+        
+        dp=[0]*(n+1)
+        dp[1]=1
+        dp[2]=2
+        
+        for i in range(3,n+1):
+            dp[i]=dp[i-2] +dp[i-1]
+        
+        return dp[n]
+        
+        
+opt = Solution()
+print(opt.climbStairs(n = 2))
