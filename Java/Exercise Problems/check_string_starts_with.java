4d62746dc01fcabaf135e9207d26920b158f6627
saraswatrupendra <67823013+saraswatrupendra@users.noreply.github.com>
2021-10-13 23:49:17 +0530

    Update check_string_starts_with.java

--- Java/Exercise Problems/check_string_starts_with.java
+++ Java/Exercise Problems/check_string_starts_with.java
@@ -1,2 +1,9 @@
 string = "w3resource.com"
 print(string.startswith("w3r"))
+
+  
+  ////or we can do like this also
+  
+  str="w3resource.com"
+  strStart="w3c"
+  print(strStart.equals(str.substring(0,strStart.length())));
