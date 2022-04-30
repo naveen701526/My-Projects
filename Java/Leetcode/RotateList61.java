847f1e1c4a802779772469e6db2f1c6b458f7716
naveen <neonew8984@gmail.com>
2022-04-30 10:31:28 +0530

    edited rotateList 61

--- Java/Leetcode/RotateList61.java
+++ Java/Leetcode/RotateList61.java
@@ -1,33 +1,33 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-        if (k <= 0 || head == null || head.next == null) {
+    public ListNode rotateRight(ListNode head, int key) {
+        if (key <= 0 || head == null || head.next == null) {
             return head;
         }
-        ListNode temp=head;
-        int length=1;
-        while(temp.next!=null){
-            temp=temp.next;
+        ListNode temp = head;
+        int length = 1;
+        while (temp.next != null) {
+            temp = temp.next;
             length++;
         }
-        temp.next=head;
-        int mod=k%length;
-        int loop=length-mod;
-        ListNode dummy=head;
-         for (int i = 0; i < loop - 1; i++) {
-            dummy=dummy.next;
+        temp.next = head;
+        int mod = key % length;
+        int loop = length - mod;
+        ListNode dummy = head;
+        for (int i = 0; i < loop - 1; i++) {
+            dummy = dummy.next;
         }
-        head=dummy.next;
-        dummy.next=null;
+        head = dummy.next;
+        dummy.next = null;
         return head;
     }
 }
