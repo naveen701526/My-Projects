a2db667cdd1c5f05132d2abc289bf9eff7c1e267
linkedinuser <115945177+linkedinuser@users.noreply.github.com>
2022-10-16 23:20:54 +0530

    Create PermutationSequence60.java

---
+++ Java/Leetcode/PermutationSequence60.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> num = new LinkedList<Integer>();
+        for (int i = 1; i <= n; i++) num.add(i);
+        int[] fact = new int[n];  // factorial
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) fact[i] = i*fact[i-1];
+        k = k-1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i > 0; i--){
+            int ind = k/fact[i-1];
+            k = k%fact[i-1];
+            sb.append(num.get(ind));
+            num.remove(ind);
+        }
+        return sb.toString();
+    }
+}
