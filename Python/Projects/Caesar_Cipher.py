b5d0d07c901a69b4b84bb996ee9e593950a55fc8
YashikaGupta03 <116101406+YashikaGupta03@users.noreply.github.com>
2022-10-28 00:40:32 +0530

    Create Caesar_Cipher.py
    Created Cipher Generator in python.

---
+++ Python/Projects/Caesar_Cipher.py
@@ -0,0 +1,59 @@
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 
+          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+lower_charset=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+               'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+upper_charset=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+               'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+
+def caesar(text,shift,direction):
+    end_text=""
+    if direction=="decode" :
+        shift*=-1
+    for c in text :
+        if c in lower_charset :
+            position = lower_charset.index(c)
+            new_position = position + shift
+            end_text += lower_charset[new_position]
+        elif c in upper_charset:
+            position = upper_charset.index(c)
+            new_position = position + shift
+            end_text += upper_charset[new_position]
+        else :
+            end_text+=c
+    print(f"Here's the {direction}d result : {end_text}")
+
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                   
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+run=True
+while run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt : ")
+    text = input("Type your message : ")
+    shift = int(input("Type the shift number : "))
+    shift=shift%26
+
+    caesar(text,shift,direction)
+
+    run_again=input("Type 'yes' if you want to go again. Otherwise type 'no' : ")
+    if(run_again=="no"):
+        run=False
+        print("Good Bye!!")
